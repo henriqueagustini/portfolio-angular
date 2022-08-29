d38b15aec9b79d37128f097fa6d7d6cc782e1311
Henrique Agustini <henrique80@gmail.com>
2022-08-29 16:06:53 -0300

    Organizando páginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ResumeComponent } from './resume/resume.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResumeComponent,
+    PortfolioComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
